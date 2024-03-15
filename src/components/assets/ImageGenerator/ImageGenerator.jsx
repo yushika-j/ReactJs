15f85fb0fc712cfb6550b335f5fe83257387c625
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-14 21:21:15 -0400

    created files

---
+++ src/components/assets/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ImageGenerator.css'
+const ImageGenerator = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default ImageGenerator
