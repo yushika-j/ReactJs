d27cd2994abfb9f8b65e21d77cff4be30492a243
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-14 21:31:02 -0400

    updated App.jsx

--- src/components/assets/ImageGenerator/ImageGenerator.jsx
+++ src/components/assets/ImageGenerator/ImageGenerator.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './ImageGenerator.css'
+import default_image from '../assets/image-ai.jpeg'
+
+
 const ImageGenerator = () => {
     return (
-        <div>
-            
+        <div class= 'ai-img-generator'>
+            <div class='header'>
+                AI image <span>generator</span>
+            </div>
         </div>
     )
 
